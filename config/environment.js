ed9408c47828c62c23d9288396fba082cb9ebd7b
Jamie White <jameswhite@salesforce.com>
2020-02-04 22:49:29 +0100

    empty root url

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'octane-concurrency',
     environment,
-    rootURL: '/',
+    rootURL: '',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
